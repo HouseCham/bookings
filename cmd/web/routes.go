c93ee3d13bc137e9bb2adee98c389915d9869c09
HouseCham <ramsesramirezvallejo@gmail.com>
2023-03-05 16:39:08 -0600

    Go templates created

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,6 +22,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/make-reservation", handlers.Repo.MakeReservation)
 
 	// File server -> in order to pull files from the project like images, css & js
 	fileServer := http.FileServer(http.Dir("./src/"))
